484d6f0c66dc7445003d0c915a226d4b688a0b73
Ben <71402472+btims512@users.noreply.github.com>
2023-05-05 10:34:34 -0600

    1st graph push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+import data from "./data";
+
+function flattenData(data) {
+  return data.map(([_, __, entry]) => {
+    const { Planet = {}, Satellite = {} } = entry;
+
+    return {
+      time: Planet.time || Satellite.time,
+      planetX: Planet.x || 0,
+      planetY: Planet.y || 0,
+      satelliteX: Satellite.x || 0,
+      satelliteY: Satellite.y || 0,
+    };
+  });
+}
 
 function App() {
+  const flattenedData = flattenData(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sedaro Nano Visualization</h1>
+      <LineChart
+        width={500}
+        height={300}
+        data={flattenedData}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="time" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="planetX"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+        <Line type="monotone" dataKey="planetY" stroke="#82ca9d" />
+        <Line type="monotone" dataKey="satelliteX" stroke="#ffc658" />
+        <Line type="monotone" dataKey="satelliteY" stroke="#ff7300" />
+      </LineChart>
     </div>
   );
 }
